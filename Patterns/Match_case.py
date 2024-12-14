0069075874fd89fd4795014dd7b0c50d8b507876
bodik <killer1992bodik@gmail.com>
2024-12-14 16:22:48 +0200

    Done Lesson3_task1

---
+++ Patterns/Match_case.py
@@ -0,0 +1,14 @@
+number1 = int(input('Enter first number: '))
+action = int(input('1. +\n2. -\n3. *\n4. /'))
+number2 = int(input('Enter second number: '))
+match action:
+    case 1:
+        print(number1 + number2)
+    case 2:
+        print(number1 - number2)
+    case 3:
+        print(number1 * number2)
+    case 4:
+        print(number1 / number2)
+    case _:
+        print('Invalid value')
