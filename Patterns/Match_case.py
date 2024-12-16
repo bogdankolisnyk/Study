e4298aca30180ad4961c4d02b97f6013eae623e2
bodik <killer1992bodik@gmail.com>
2024-12-16 12:28:32 +0200

    Done Lesson3_task1

--- Patterns/Match_case.py
+++ Patterns/Match_case.py
@@ -1,6 +1,7 @@
 number1 = int(input('Enter first number: '))
 action = int(input('1. +\n2. -\n3. *\n4. /'))
 number2 = int(input('Enter second number: '))
+
 match action:
     case 1:
         print(number1 + number2)
@@ -9,6 +10,8 @@ match action:
     case 3:
         print(number1 * number2)
     case 4:
+        if number2 == 0:
+            print('Divide for 0 PROHIBIT')
         print(number1 / number2)
     case _:
         print('Invalid value')
