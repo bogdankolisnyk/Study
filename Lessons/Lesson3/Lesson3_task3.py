532d9212c3106fdfc31eaabc329b402053e80aba
bodik <killer1992bodik@gmail.com>
2024-12-16 17:41:19 +0200

    Done Lesson3_task1

---
+++ Lessons/Lesson3/Lesson3_task3.py
@@ -0,0 +1,6 @@
+numbers = [1,3,4,5,6,5,6]
+first_part_of_list = round(len(numbers) / 2)
+second_part_of_list = len(numbers) // 2
+if first_part_of_list > second_part_of_list:
+    print()
+print(first_part_of_list, second_part_of_list)
