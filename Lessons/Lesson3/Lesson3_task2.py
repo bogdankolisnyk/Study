86cc1d6f8a72e6db4ae03f846c57c01ec1bfc278
bodik <killer1992bodik@gmail.com>
2024-12-16 16:38:45 +0200

    Done Lesson3_task1

---
+++ Lessons/Lesson3/Lesson3_task2.py
@@ -0,0 +1,2 @@
+numbers = [2,6,4,55,7,9,12]
+print(numbers[-1::6] + numbers[0:6])
--- Patterns/Match_case.py
+++ Patterns/Match_case.py
@@ -1,25 +1,25 @@
-number1 = int(input('Enter first number: '))
-action = int(input('1. +\n2. -\n3. *\n4. /'))
-number2 = int(input('Enter second number: '))
-
-match action:
-    case 1:
-        print(number1 + number2)
-    case 2:
-        print(number1 - number2)
-    case 3:
-        print(number1 * number2)
-    case 4:
-        if number2 == 0:
-            print('Divide for 0 PROHIBIT')
-        print(number1 / number2)
-    case _:
-        print('Invalid value')
+# number1 = int(input('Enter first number: '))
+# action = int(input('1. +\n2. -\n3. *\n4. /'))
+# number2 = int(input('Enter second number: '))
+#
+# match action:
+#     case 1:
+#         print(number1 + number2)
+#     case 2:
+#         print(number1 - number2)
+#     case 3:
+#         print(number1 * number2)
+#     case 4:
+#         if number2 == 0:
+#             print('Divide for 0 PROHIBIT')
+#         print(number1 / number2)
+#     case _:
+#         print('Invalid value')
 
 # number = (1, 8, 5, 7)
 # print(number[-3])
-# numbers = [2,6,4,55,7,9,12]
-# print(numbers[1:5])
+numbers = [2,6,4,55,7,9,12]
+print(numbers[-1::6] + numbers[0:6])
 # print(numbers[-1])
 # print(numbers[1:5:1])
 # print(numbers[::-1])
@@ -29,3 +29,15 @@ match action:
 # print(name1)
 # print(name2)
 # print(names)
+
+# numbers = [1, 1, 2, 5, 77, 1, 6, 1]
+# # numbers.remove(1)
+# # print(numbers.index(1))
+# # print(numbers.pop(3))
+# # print(numbers)
+# # print(numbers.count(1))
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# numbers_sorted = sorted(numbers)
+# print(numbers_sorted)
