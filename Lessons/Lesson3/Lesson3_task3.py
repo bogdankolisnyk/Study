8acfa4c0edc31aadf0cb1ee8e8918711278b083e
bodik <killer1992bodik@gmail.com>
2024-12-16 18:12:51 +0200

    Done Lesson3_task1

--- Lessons/Lesson3/Lesson3_task3.py
+++ Lessons/Lesson3/Lesson3_task3.py
@@ -7,6 +7,5 @@ elif first_part_of_list > second_part_of_list or first_part_of_list == 0:
     print(numbers, '=>', '[',numbers[0:first_part_of_list],',',numbers[second_part_of_list+1:len(numbers)],']')
 elif first_part_of_list == second_part_of_list:
     print(numbers, '=>', '[',numbers[0:first_part_of_list],',',numbers[second_part_of_list:len(numbers)],']')
-# elif first_part_of_list == second_part_of_list == 0:
 else:
     print(numbers[first_part_of_list], numbers[second_part_of_list])
