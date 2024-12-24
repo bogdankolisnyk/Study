236cc32185cbf6b17d50990e30f33d5861a3b9ce
bodik <killer1992bodik@gmail.com>
2024-12-24 18:22:58 +0200

    Done Lesson3_task1

--- Lessons/Lesson4/Lesson4_task3.py
+++ Lessons/Lesson4/Lesson4_task3.py
@@ -3,10 +3,9 @@ import copy
 numbers = []
 for i in range(random.randint(3, 10)):
     numbers.append(i)
-print(numbers)
 res = []
 first_digit = res.append(numbers[0])
 second = res.append(numbers[2])
 third_digit = res.append(numbers[-2])
-print(res)
+print(numbers, '==', res)
 
