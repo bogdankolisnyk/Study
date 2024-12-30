0877e4f32a9e702085f80ba3d68d698234c44292
bodik <killer1992bodik@gmail.com>
2024-12-30 12:22:54 +0200

    Done Lesson3_task1

--- Lessons/Lesson5/Lesson5_task3.py
+++ Lessons/Lesson5/Lesson5_task3.py
@@ -1,14 +1,11 @@
 import string
 
-def create_hashtag(text):
-    text = text.translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
-    words = text.split()
-    hashtag = ''.join(["{}".format(word.capitalize()) for word in words])
+text = input("Enter a string: ")
+text = text.translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
+words = text.split()
+hashtag = ''.join([word.capitalize() for word in words])
 
-    if len(hashtag) > 140:
-        hashtag = hashtag[:140]
-    return "#{}".format(hashtag)
-
-input_text = input("Enter a string: ")
-result = create_hashtag(input_text)
-print(result)
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+hashtag = "#" + hashtag
+print(hashtag)
