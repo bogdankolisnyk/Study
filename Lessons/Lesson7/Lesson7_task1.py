6279bf7af48f331f6fe642de2c69ec04ca989975
bodik <killer1992bodik@gmail.com>
2024-12-30 15:34:18 +0200

    Done Lesson3_task1

---
+++ Lessons/Lesson7/Lesson7_task1.py
@@ -0,0 +1,8 @@
+def say_hi(name, age):
+    return f"Hello, my name is {name} and I am {age} years old."
+
+assert say_hi("Alex", 32) == "Hello, my name is Alex and I am 32 years old."
+print(say_hi("Alex", 32))
+assert say_hi("Frank", 68) == "Hello, my name is Frank and I am 68 years old."
+print(say_hi("Frank", 68))
+print('ОК')
