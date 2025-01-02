a4cf32e19957de887e2720c7806e2b2c8338ae49
bodik <killer1992bodik@gmail.com>
2025-01-02 12:35:25 +0200

    Done Lesson3_task1

--- Lessons/Lesson7/Lesson7_task3.py
+++ Lessons/Lesson7/Lesson7_task3.py
@@ -6,8 +6,8 @@ def second_index(text, some_str):
     second_index = text.find(some_str, first_index + 1)
     return second_index if second_index != -1 else None
 
-assert second_index("sims", "s") == 3, 'Test1'
-assert second_index("find the river", "e") == 12, 'Test2'
-assert second_index("hi", "h") is None, 'Test3'
-assert second_index("Hello, hello", "lo") == 10, 'Test4'
+assert second_index("sims", "s") == 3
+assert second_index("find the river", "e") == 12
+assert second_index("hi", "h") is None
+assert second_index("Hello, hello", "lo") == 10
 print('ОК')
